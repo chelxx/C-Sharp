03cc79992c383f28a8679ced7110fab14c935610
Chelsea Cabalan <chex239@gmail.com>
2018-03-05 15:11:41 -0600

    C# Assignments

---
+++ collections_prac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace collections_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
