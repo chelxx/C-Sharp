b78f47246aa44406955f03ceaa19178264cc7df6
Chelsea Cabalan <chex239@gmail.com>
2018-03-05 22:23:45 -0600

    C# Assignments

--- basic_13/Program.cs
+++ basic_13/Program.cs
@@ -35,108 +35,145 @@ namespace basic_13
             }
         }
         // #4 Iterating through an Array
-        public static void Iterate_Array()
+        public static void Iterate_Array(int[] arr)
         {
-            int[] numberArray = {1,3,5,7,9,13};
-            for(int i = 0; i < numberArray.Length; i++){
-                    Console.WriteLine(numberArray[i]);
+            for(int i = 0; i < arr.Length; i++){
+                    Console.WriteLine(arr[i]);
             }
         }
         // #5 Find Max
-        public static void Find_Max()
+        public static void Find_Max(int[] arr)
         {
-            int[] max_Array = {1,2,3,14,5};
-            int max = max_Array[0];
-            for(int i = 0; i < max_Array.Length; i++)
+            int max = arr[0];
+            for(int i = 0; i < arr.Length; i++)
             {
-                if (max < max_Array[i])
+                if (max < arr[i])
                 {
-                    max = max_Array[i];
+                    max = arr[i];
                 }
             }
                 Console.WriteLine("The Max Number is: " + max);
         }
         // #6 Get Average
-        public static void Get_Average()
+        public static void Get_Average(int[] arr)
         {
-            int[] avg_Array = {1,2,3,4,5};
             int sum = 0;
-            for (int i = 0; i < avg_Array.Length + 1; i++)
+            for (int i = 0; i < arr.Length + 1; i++)
             {
                 sum = sum + i;
             }
-            int avg = sum / avg_Array.Length;
+            int avg = sum / arr.Length;
             Console.WriteLine("The Average Number is: " + avg);
         }
         // #7 Array with Odd Numbers
         public static void Odd_Numbers()
         {
-            List<int> odd_List = new List<int>();
-            for(int i = 0; i <= 255; i++)
-            {
-                if(i % 2 != 0)
-                {
-                    odd_List.Add(i);
-                }
-            }
-            int[] y = odd_List.ToArray();
-            Console.Write("[");
-            foreach(var odd in odd_List)
-            {
-                Console.Write(odd.ToString());
-                if(odd != 255)
-                {
-                    Console.Write(", "); // Replaces the Even Number
-                }
-            }
-            Console.Write("]");
+
         }
         // #8 Greater than Y
-        public static void Greater_Than_Y()
+        public static void Greater_Than_Y(int[] arr, int y)
         {
-
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > y)
+                {
+                    count = count + 1;
+                }
+            }
+            Console.WriteLine("There are " + count + " numbers greater than Y.");
         }
         // #9 Square the Values
-        public static void Square_Values()
+        public static void Square_Values(int[] arr)
         {
-
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i] * arr[i];
+                Console.WriteLine(arr[i]);
+            }
         }
         // #10 Eliminate Negative Numbers
         public static void No_Negatives()
         {
-
+            int[] no_Negs = new int[6] {1,2,3,14,5,-88};
+            for (int i = 0; i < no_Negs.Length; i++)
+            {
+                if (no_Negs[i] < 0)
+                {
+                    no_Negs[i] = 0;
+                }
+                Console.WriteLine(no_Negs[i]);
+            }
         }
         // #11 Min, Max, and Average
-        public static void Find_Min_Max_Avg()
+        public static void Find_Min_Max_Avg(int[] arr)
         {
-
+            int max = arr[0];
+            int min = arr[0];
+            int sum = 0;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                }
+                sum = sum + arr[i];
+            }
+            int avg = sum / arr.Length;
+            Console.WriteLine("The results are: Min is " + min + ", Max is " + max + ", Sum is " + sum + ", and Avg is " + avg);
         }
         // #12 Shifting the values in an array
-        public static void Shifting_Values()
+        public static void Shifting_Values(int[] arr)
         {
-
+            List<int> shift = new List<int>();
+        
+            for(int i = 0; i < arr.Length-1; i++){
+                shift.Add(arr[i+1]);
+            }
+            shift.Add(0);
+            foreach(var thing in shift){
+                Console.WriteLine(thing);
+            }
         }
         // #13 Number to String
         public static void Number_To_String()
         {
+            List<string> templist = new List<string>();
 
+            for(int i = 0; i < numberArray.Length; i++){
+                if(numberArray[i] < 0){
+                    templist.Add("Dojo");
+                } else {
+                    templist.Add(numberArray[i].ToString());
+                }
+            }
+            // templist.ToArray();
+            foreach(var item in templist){
+                Console.WriteLine(item);
+            }
+            return templist;
         }
         // Calling the Functions
         public static void Main(string[] args)
         {
-            int[] the_Array = {1,2,3,14,5,-88};
             Print_1_to_255();
             Print_Odds_1_to_255();
             Print_Sum();
-            Iterate_Array();
-            Find_Max();
-            Get_Average();
-            Odd_Numbers();
-            Greater_Than_Y();
-            Square_Values();
-            No_Negatives();
-            Find_Min_Max_Avg();
-            Shifting_Values();
+            int[] the_Array = new int[4] {1,5,10,-2};
+            Iterate_Array(the_Array);
+            Find_Max(the_Array);
+            Get_Average(the_Array);
+            Odd_Numbers(); // Needs work
+            Greater_Than_Y(the_Array, 2);
+            Square_Values(the_Array);
+            No_Negatives(); // This has its own array
+            Find_Min_Max_Avg(the_Array);
+            Shifting_Values(the_Array);
+            //object here
             Number_To_String();
         }
     }
