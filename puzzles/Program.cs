7d6a3e7eb35c9b8b4587415ed24c24383e979ad0
Chelsea Cabalan <chex239@gmail.com>
2018-03-06 12:50:34 -0600

    C# Assignments

---
+++ puzzles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace puzzles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Puzzles
+            // Random Array
+            Random rand = new Random();
+
+        }
+    }
+}
