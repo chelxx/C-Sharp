99dc94092d7b79098942cba8b2b8f25d255ae6e8
Chelsea Cabalan <chex239@gmail.com>
2018-03-08 14:05:57 -0600

    C# Assignments

--- calling_card/Controllers/HelloController.cs
+++ calling_card/Controllers/HelloController.cs
@@ -5,57 +5,6 @@ namespace calling_card.Controllers
 {
     public class HelloController : Controller
     {
-        // [HttpGet]
-        // [Route("")]
-        // public string Home()
-        // {
-        //     return "Hello, this is my Calling Card assignment!";
-        // }
-
-        // [HttpGet]
-        // [Route("{firstname}")]
-        // public JsonResult FirstName(string firstname)
-        // {
-        //     var AnonObject = new
-        //     {
-        //         FirstName = firstname
-        //     };
-        //     return Json (AnonObject);
-        // }
-
-        // [HttpGet]
-        // [Route("{lastname}")]
-        // public JsonResult LastName(string lastname)
-        // {
-        //     var AnonObject = new
-        //     {
-        //         LastName = lastname
-        //     };
-        //     return Json (AnonObject);
-        // }
-
-        // [HttpGet]
-        // [Route("{age}")]
-        // public JsonResult Age(int age)
-        // {
-        //     var AnonObject = new
-        //     {
-        //         Age = age
-        //     };
-        //     return Json (AnonObject);
-        // }
-
-        // [HttpGet]
-        // [Route("{favecolor}")]
-        // public JsonResult FaveColor(string favecolor)
-        // {
-        //     var AnonObject = new
-        //     {
-        //         FaveColor = favecolor
-        //     };
-        //     return Json (AnonObject);
-        // }
-
         [HttpGet]
         [Route("{firstname}/{lastname}/{age}/{favecolor}")]
         public JsonResult Display(string firstname, string lastname, int age, string favecolor)
