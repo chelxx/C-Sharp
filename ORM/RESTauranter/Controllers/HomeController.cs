a25ab25af50ff19ccc23a1249358eeb9aac55c39
Chelsea Cabalan <chex239@gmail.com>
2018-03-19 21:44:09 -0500

    C# Assignments

---
+++ ORM/RESTauranter/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RESTauranter.Models;
+
+namespace RESTauranter.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private RestaurantContext _context;
+        public HomeController(RestaurantContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewBag.errors = ModelState.Values;
+            return View("Index");
+        }
+
+        [HttpPost]
+        [Route("createReview")]
+        public IActionResult createReview(Restaurant newRestaurant)
+        {   
+            if(ModelState.IsValid)
+            {
+                _context.Add(newRestaurant);
+                _context.SaveChanges();
+                return RedirectToAction("Review");
+            }
+            else
+            {
+                return View("Index");
+            }
+        }   
+
+        [HttpGet]
+        [Route("reviews")]
+        public IActionResult Review()
+        {
+            List<Restaurant> rName = _context.restaurant.ToList();
+            ViewBag.Reviews = rName.OrderByDescending(r => r.DateVisit);
+            return View("Reviews");
+        }
+    }
+}
---
+++ ORM/RESTauranter/Models/Restaurant.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace RESTauranter.Models
+{
+    public class ValidDateAttribute : ValidationAttribute 
+    {
+        public override bool IsValid (object value) 
+        {
+            DateTime d = Convert.ToDateTime (value);
+            return d <= DateTime.Now;
+        }
+    }
+    public abstract class BaseEntity
+    {
+        // EMPTY!!!
+    }
+
+    public class Restaurant : BaseEntity
+    {
+        [Key]
+        public int RestaurantID {get;set;}
+
+        [Required]
+        [MinLength(2, ErrorMessage="Must be filled out!")]
+        public string ReviewerName {get;set;}
+
+        [Required]
+        [MinLength(2, ErrorMessage="Must be filled out!")]
+        public string RestaurantName {get;set;}
+        [Required]
+        [MinLength(10, ErrorMessage="Must be filled out!")]
+        public string Review {get;set;}
+
+        [Required]
+        [ValidDate(ErrorMessage ="Date can not be in the future!")]
+        public DateTime DateVisit {get;set;}
+
+        [Required]
+        public int Stars {get;set;}
+    }
+}
